ad0872018e5bc3b74d45058cbb3ddddb01cc28e9
contact@gladosx.tv <contact@gladosx.tv>
2014-04-25 23:59:22 +0200

    preparation

---
+++ Project/Scripts/src/MakeAtlasSprite.java
@@ -0,0 +1,7 @@
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+public class MakeAtlasSprite {
+        public static void main (String[] args) throws Exception {
+ 
+        	TexturePacker.process("/home/bussiere/Workspace/Saikoro/Vrac/Alphabet/Atlas", "/home/bussiere/Workspace/Saikoro/GDX/Assets/", "Alphabet1");
+        }
+}
