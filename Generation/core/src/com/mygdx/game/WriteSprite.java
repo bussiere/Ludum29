c2d13aa240256196223190aa8559889ef5e15a9a
contact@gladosx.tv <contact@gladosx.tv>
2014-04-26 17:39:50 +0200

    dev2

--- Generation/core/src/com/mygdx/game/WriteSprite.java
+++ Generation/core/src/com/mygdx/game/WriteSprite.java
@@ -3,6 +3,8 @@ package com.mygdx.game;
 public class WriteSprite {
 
 	int size;
+	int xPlacement;
+	int yPlacement;
 	
 	public WriteSprite()
 	{
@@ -11,12 +13,46 @@ public class WriteSprite {
 	
 	public void write(String sentence)
 	{
+		write(sentence,this.xPlacement,this.yPlacement);
+	}
+	
+	public void write(String sentence,int xPlacement, int yPlacement)
+	{
+		write(sentence,this.xPlacement,this.yPlacement,this.size);
+	}
+	
+	public void write(String sentence,int xPlacement, int yPlacement, int size)
+	{
 		
 	}
 	
 	public void render()
 	{
 		
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public int getxPlacement() {
+		return xPlacement;
+	}
+
+	public void setxPlacement(int xPlacement) {
+		this.xPlacement = xPlacement;
+	}
+
+	public int getyPlacement() {
+		return yPlacement;
+	}
+
+	public void setyPlacement(int yPlacement) {
+		this.yPlacement = yPlacement;
 	}
 	
 }
---
+++ Project/.metadata/.plugins/org.eclipse.core.resources/.history/d6/10c5da8558cd00131b5eda03064c09af
@@ -0,0 +1,22 @@
+package com.mygdx.game;
+
+public class WriteSprite {
+
+	int size;
+	
+	public WriteSprite()
+	{
+		
+	}
+	
+	public void write(String sentence)
+	{
+		
+	}
+	
+	public void render()
+	{
+		
+	}
+	
+}
