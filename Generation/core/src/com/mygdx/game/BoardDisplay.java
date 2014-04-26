988b68efc064c222bc1ffc13e57cb3546488e843
contact@gladosx.tv <contact@gladosx.tv>
2014-04-26 17:51:18 +0200

    rajout atlas

--- Generation/core/src/com/mygdx/game/BoardDisplay.java
+++ Generation/core/src/com/mygdx/game/BoardDisplay.java
@@ -1,5 +1,11 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class BoardDisplay extends Board {
 
+	 public void render( SpriteBatch batch){
+		 writeBoard.render(batch);
+	 }
+	
 }
